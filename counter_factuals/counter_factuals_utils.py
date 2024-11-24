fae4da8eff2bfd11679ec7da66d9a73601751a46
Naterjack <20245490+Naterjack@users.noreply.github.com>
2024-11-24 18:34:32 +0000

    counter factual image generation backend

--- counter_factuals/counter_factuals_utils.py
+++ counter_factuals/counter_factuals_utils.py
@@ -10,6 +10,9 @@ from utils.config import CIFAR_10_Config, Project_Config
 from typing import List
 from pathlib import Path
 
+from diffusers import DiffusionPipeline
+import torch
+
 class Counter_Factual_Dataset_Generator(object):
     def __init__(self) -> None:
         self.DATASET_NAME = "cifar-10-original-indicies"
@@ -97,4 +100,69 @@ class Counter_Factual_Dataset_Generator(object):
         f = open(self.base_path+"README.md", "w")
         f.writelines(file_lines)
         f.close()
+
+class Counter_Factual_Image_Generator(object):
+    def __init__(self,
+                 project_config: Project_Config,
+                 counter_factual_model_name: str,
+                 is_lora: bool,
+                 ) -> None:
+        self.project_config = project_config
+        self.IS_LORA = is_lora
+        self.counter_factual_model_name = counter_factual_model_name
+    
+    def generate_counter_factual_image(self,
+                                       seed: int,
+                                       image_class: str,
+                                       save_image: bool):
+        f = self.project_config.folder_symbol
+        p = self.project_config.PWD + f + "counter_factuals" + f + self.counter_factual_model_name
+        assert(os.path.isdir(p))
+
+        if self.IS_LORA:
+            #TODO: check this
+            p = p + f + "pytorch_lora_weights.safetensors"
+            pipe = DiffusionPipeline.from_pretrained("stable-diffusion-v1-5/stable-diffusion-v1-5", torch_dtype=torch.float16)
+            if self.project_config.IS_CUDA:
+                pipe.to("cuda")
+            pipe.load_lora_weights(p)
+        else:
+            pipe = DiffusionPipeline.from_pretrained(p)
+            if self.project_config.IS_CUDA:
+                pipe.to("cuda")
         
+        #This trick is directly based of something I found from DTRAK's code, see:
+        #https://github.com/sail-sg/D-TRAK/blob/main/ArtBench5/train_text_to_image_lora.py 
+        #line 1029 (at time of writing)
+        #Notably, their version does not work as is in current versions of diffusers, 
+        # and requires this slight adaption (creating a list of bools rather than a single bool)
+        
+        def dummy(images, **kwargs):
+                return images, [False]*len(images)
+
+        pipe.safety_checker = dummy
+
+        prompts=['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'horse', 'ship', 'truck', 'frog']
+
+        assert(image_class in prompts)
+
+        #from here https://github.com/huggingface/diffusers/issues/1786#issuecomment-1359897983
+        #   If this is in the proper documentation I sure couldn't find it easily
+        #pipe.set_progress_bar_config(leave=False)
+        pipe.set_progress_bar_config(disable=True)
+        
+        generator = torch.Generator(device=("cuda" if self.project_config.IS_CUDA else "cpu")).manual_seed(seed)
+
+        image = pipe(
+            height=32,
+            width=32,
+            prompt=image_class,
+            generator=generator,
+        ).images[0]
+
+        if save_image:
+            save_path = (p + f + "images")
+            os.makedirs(save_path, exist_ok=True)
+            image.save(save_path + f + image_class + "_" + str(seed)+ ".png")
+
+        return image
